d0e37485274ad29db2f239a181aedfa52d99f4b2
izaiasvalentim <izaias451@gmail.com>
2023-06-28 21:06:26 +0044

    Mais práticas

--- ALGORITMOS_ESTRUTURAS_DADOS/PRATICAS_LAB/ponteiros_dois.c
+++ ALGORITMOS_ESTRUTURAS_DADOS/PRATICAS_LAB/ponteiros_dois.c
@@ -3,10 +3,19 @@
 int main(void){
 
     float var = 2.5;
+    
+    //Declaração de ponteiro
+    
     float *ponteiro;
+    
     ponteiro = &var;
 
-printf("var = %f \n ponteiro = %p", var, ponteiro);
+    printf("var = %f \n ponteiro = %p", var, ponteiro);
 
+    //Modificar o valor da variável var atráves do ponteiro
+    *ponteiro = 3.7;
+
+    printf("\n Var = %f \n Ponteiro = %p", var, ponteiro);
+    
     return 0;
 }
