6ee5daf7301e7a8349b2ac52acd09df331df5d15
izaiasvalentim <izaias451@gmail.com>
2023-07-03 10:56:39 -0300

    Informações Q4

--- ALGORITMOS_ESTRUTURAS_DADOS/LISTAS/LISTA_PONTEIROS/Q4.c
+++ ALGORITMOS_ESTRUTURAS_DADOS/LISTAS/LISTA_PONTEIROS/Q4.c
@@ -19,7 +19,21 @@ int main(void){
 }
 
 void calcula_hexagono(float l, float *area, float *perimetro){
+    
+    /*
+        Função que calcula a área e perímetro de um hexágono regular.
 
+        float l : lado, valor decimal.
+        float *area: ponteiro do tipo real.
+        float *perimetro: ponteiro do tipo real.    
+
+        Entrada: lado(fornecido pelo usuário), endereço para area, endereço para o perímetro.
+
+        ação: cálculo da área é armazenado no endereço do ponteiro da area.
+              cálculo da perimetro é armazenado no endereço do ponteiro do perimetro.
+             
+    */  
+   
     *area = (3*pow(l,2)*sqrt(3))/2;
     *perimetro = 6 * l;
 
