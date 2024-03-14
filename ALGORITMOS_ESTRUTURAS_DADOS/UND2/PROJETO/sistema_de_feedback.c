c7f81c0563e89b1aa6e79c3e7e50a32ec2ebc61e
Izaias Valentim <izaias451@gmail.com>
2024-03-14 15:17:08 -0300

    Criação da funcionalidade de navegação entre os menus.

--- ALGORITMOS_ESTRUTURAS_DADOS/UND2/PROJETO/sistema_de_feedback.c
+++ ALGORITMOS_ESTRUTURAS_DADOS/UND2/PROJETO/sistema_de_feedback.c
@@ -14,7 +14,99 @@ int menuUserAdmin();
 
 int main(void)
 {
+    // Variável de controle para o primeiro menu.
+    int opcao_navegacao_inicial;
     setlocale(LC_ALL, "portuguese");
+
+    printf("Bem vindo(a) a plataforma de feedback\n");
+    Sleep(1000);
+    printf("Realize o seu login para continuar");
+    Sleep(1000);
+
+    do
+    {
+        printf("\n");
+
+        // Exibe e recebe a escolha no Menu inicial.
+        opcao_navegacao_inicial = menuInicialLoginCadastro();
+
+        if (opcao_navegacao_inicial == 0)
+        {
+            break;
+        }
+
+        // Area Login
+        else if (opcao_navegacao_inicial == 1)
+        {
+
+            // Variável de controle para a tela de Login.
+            int status_login;
+            status_login = menuLogin();
+
+            if (status_login == 1)
+            {
+                printf("\nBem vindo usuário, a seguir você terá acesso a área de feedback\n");
+                Sleep(2000);
+
+                int escolha_comum;
+
+                do
+                {
+
+                    escolha_comum = menuUserComum();
+                    // Funções do menuUsuario.
+                } while (escolha_comum != 0);
+            }
+
+            else if (status_login == 2)
+            {
+                printf("\nBem vindo admin, a seguir você terá acesso a área de controle\n");
+                Sleep(2000);
+
+                int escolha_admin;
+
+                do
+                {
+                    escolha_admin = menuUserAdmin();
+                    // Funções do menu Admin;
+                } while (escolha_admin != 0);
+            }
+
+            else
+            {
+                printf("Falha no login");
+            }
+
+            // Retoma a execução no menu inicial.
+            continue;
+        }
+
+        // Cadastro usuario.
+        else if (opcao_navegacao_inicial == 2)
+        {
+            int status_cadastro = cadastroUsuario();
+            if (status_cadastro == 0)
+            {
+                printf("Cadastro realizado com sucesso!\n");
+
+                // Retoma a execução do menu inicial.
+                continue;
+            }
+            else
+            {
+                printf("falha no cadastro\n");
+
+                // Retoma a execução no menu inicial.
+                continue;
+            }
+        }
+        else
+        {
+            printf("Informe uma opção válida");
+        }
+
+    } while (opcao_navegacao_inicial != 0);
+
     return 0;
 }
 
