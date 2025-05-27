e8a407ad11ca9e4a8d014dada01d7ebc9723b2df
Luis Izaías Valentim De Aquino <118863023+IzaiasValentim@users.noreply.github.com>
2025-05-27 10:11:58 -0300

    Update model.h

--- COMPUTACAO_GRAFICA/renderizacao_3d/include/model.h
+++ COMPUTACAO_GRAFICA/renderizacao_3d/include/model.h
@@ -21,12 +21,15 @@
 #define MAX_VERTICES 50000
 #define MAX_FACES 50000
 #define MAX_FACE_VERTS 32
+#define M_PI 3.14159265358979323846
 
-typedef struct {
+typedef struct
+{
   float x, y, z;
 } Vertex;
 
-typedef struct {
+typedef struct
+{
   int verts[MAX_FACE_VERTS];
   int n;
 } Face;
@@ -39,11 +42,21 @@ void save();
 
 void draw_line(int x0, int y0, int x1, int y1);
 
-int load_obj(const char *filename, Vertex *vertices, int *vcount, Face *faces, int *fcount);
+int load_obj(const char *filename, Vertex *vertices, int *vcount,
+             Face *faces, int *fcount, int rotationAngle, float scaleFactor, int translate, int quadrant, char axis, int shear_value);
 
-void resizing( Vertex v0, Vertex v1 );
+void resizing(Vertex v0, Vertex v1);
 
 void render_faces(Vertex *vertices, Face *faces, int vcount, int fcount);
 
+Vertex get_center(Vertex *vertices, int count);
 
+void translate_vertices(Vertex *vertices, int count, int translate);
+
+void rotate_and_scale_vertices(Vertex *src, Vertex *dst, int count,
+                               Vertex center, float angleRadians, float scaleFactor);
+
+void reflect_model(Vertex *vertices, int numVertices, int quadrant);
+
+void shear_model(Vertex *vertices, int numVertices, char axis, int shear_value);
 #endif
